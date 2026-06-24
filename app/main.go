460821d32c36d416990ecd3b506f6d351bd8bc16
Kevin De Coninck <kevin.dconinck@gmail.com>
2026-06-24 10:37:03 +0200

    Setup: Go
    - Initialize the 'app/' folder as a Go module.
    - Add the 'main' entry point for the application.

---
+++ app/main.go
@@ -0,0 +1,11 @@
+// =====================================================================================================================
+// == LICENSE:                 Copyright (c) 2026 Kevin De Coninck.
+// == SPDX-License-Identifier: LicenseRef-PolyForm-Noncommercial-1.0.0
+// =====================================================================================================================
+
+// Package main implements 'Spot', a high-performance, language-agnostic static analysis engine.
+package main
+
+func main() {
+	// TODO: Implement this function.
+}
