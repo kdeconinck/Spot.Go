7702d477ec2445b3b73e843482aa79a7e976d261
Kevin De Coninck <kevin.dconinck@gmail.com>
2026-06-25 08:30:13 +0200

    Add: Package 'syntax'
    - Add 'Token' struct. A lexical token from a Spot DSL source file.
    - Add 'TokenKind'. The syntactic role of a DSL token.

---
+++ app/syntax/token.go
@@ -0,0 +1,32 @@
+// =====================================================================================================================
+// == LICENSE:                 Copyright (c) 2026 Kevin De Coninck.
+// == SPDX-License-Identifier: LicenseRef-PolyForm-Noncommercial-1.0.0
+// =====================================================================================================================
+
+// Package syntax defines data structures that represent Spot DSL syntax.
+package syntax
+
+import "github.com/kdeconinck/spot/location"
+
+// TokenKind identifies the syntactic role of a DSL token.
+type TokenKind uint8
+
+const (
+	// TokenInvalid marks source text that cannot be tokenized as valid DSL syntax.
+	TokenInvalid TokenKind = iota
+
+	// TokenEOF marks the end of a token stream.
+	TokenEOF
+)
+
+// Token is a lexical token from a Spot DSL source file.
+type Token struct {
+	// Kind identifies the syntactic role of the token.
+	Kind TokenKind
+
+	// Text is the exact source text covered by the token.
+	Text string
+
+	// Span is the byte range covered by the token.
+	Span location.Span
+}
